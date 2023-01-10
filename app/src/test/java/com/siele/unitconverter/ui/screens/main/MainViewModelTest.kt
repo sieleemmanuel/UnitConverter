f1c001afa41b263f6b5892c4e3ad929c3c377cf7
kimsiele <sielekim.emmanuel@gmail.com>
2023-01-10 22:56:38 +0300

    Feat: add unit test for MainViewModel class

--- app/src/test/java/com/siele/unitconverter/ui/screens/main/MainViewModelTest.kt
+++ app/src/test/java/com/siele/unitconverter/ui/screens/main/MainViewModelTest.kt
@@ -1,9 +1,8 @@
 package com.siele.unitconverter.ui.screens.main
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import com.siele.unitconverter.data.api.ConverterApi
 import com.siele.unitconverter.data.model.ConversionResponse
-import com.siele.unitconverter.data.repository.ConverterRepository
+import com.siele.unitconverter.data.model.CurrencyResponse
 import com.siele.unitconverter.data.repository.DefaultConverterRepository
 import com.siele.unitconverter.ui.getOrAwaitValue
 import com.siele.unitconverter.util.Resource
@@ -11,35 +10,34 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.core.IsEqual
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.mockito.Mock
-import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
 import org.mockito.MockitoAnnotations
 
 @ExperimentalCoroutinesApi
 class MainViewModelTest{
     private lateinit var viewModel: MainViewModel
-    private lateinit var converterRepository: ConverterRepository
+    private lateinit var converterRepository: DefaultConverterRepository
 
     private val testDispatcher = StandardTestDispatcher()
     private lateinit var conversionResponse:ConversionResponse
-
-    @Mock
-    lateinit var converterApi: ConverterApi
+    private lateinit var currencyResponse:CurrencyResponse
 
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
 
     @Before
     fun setUp() {
-        MockitoAnnotations.initMocks(this)
+        MockitoAnnotations.openMocks(this)
         Dispatchers.setMain(testDispatcher)
-        converterRepository = DefaultConverterRepository(converterApi)
+        converterRepository = mock(DefaultConverterRepository::class.java)
         viewModel = MainViewModel(converterRepository)
 
         conversionResponse =
@@ -51,43 +49,59 @@ class MainViewModelTest{
                 "KES",
                 true
             )
+        currencyResponse = CurrencyResponse(
+            new_amount = 123.05,
+            new_currency = "KES",
+            old_currency = "USD",
+            old_amount = 1.0
+        )
     }
 
     @Test
-    fun getValue_InvalidInputs_returnError() {
-        runBlocking {
-            Mockito.`when`(converterRepository.getConvertedValue("", "USD", "KES"))
-                .thenReturn(Resource.Error("An error occurred, An error occurred, enter a valid value", null))
-            //viewModel.getValue("", "USD", "KES")
-            viewModel._responseState.postValue(Resource.Error("An error occurred, enter a valid value", null))
+    fun `get converted value with invalid inputs should return response error`() {
+        runTest {
+            `when`(converterRepository.getConvertedValue("", "USD", "KES", true))
+                .thenReturn(Resource.Error("An error occurred, enter a valid value", null))
+            viewModel.getValue("", "USD", "KES", true)
+            testDispatcher.scheduler.advanceUntilIdle()
             val response = viewModel.responseState.getOrAwaitValue()
             assertThat(response.message, IsEqual( Resource.Error("An error occurred, enter a valid value", null).message))
         }
     }
 
     @Test
-    fun getValue_NetworkError_returnError() {
+    fun `get converted value with no network should return network response error`() {
         runBlocking {
-            Mockito.`when`(converterRepository.getConvertedValue("", "USD", "KES"))
+            `when`(converterRepository.getConvertedValue("1", "USD", "KES", false))
                 .thenReturn(Resource.Error("Network error, check your network connection", null))
-
-            viewModel._responseState.postValue(Resource.Error("Network error, check your network connection", null))
+           viewModel.getValue("1", "USD", "KES", false)
+            testDispatcher.scheduler.advanceUntilIdle()
             val response = viewModel.responseState.getOrAwaitValue()
             assertThat(response.message, IsEqual( Resource.Error("Network error, check your network connection", null).message))
         }
     }
 
     @Test
-    fun getConvertedValue_IsValidRequest_returnSuccessResponse() {
-        runBlocking {
-            Mockito.`when`(converterRepository.getConvertedValue("1", "USD", "KES",))
+    fun `get converted value request should return success response`() {
+        runTest {
+            `when`(converterRepository.getConvertedValue("1", "USD", "KES",true))
                 .thenReturn(Resource.Success(conversionResponse))
-
-            viewModel._responseState.postValue(Resource.Success(conversionResponse))
-            viewModel.getValue("1","USD", "KES" )
-
+            viewModel.getValue("1","USD", "KES", true )
+            testDispatcher.scheduler.advanceUntilIdle()
             val result = viewModel.responseState.getOrAwaitValue()
-            assertThat(result.data, IsEqual( Resource.Success(conversionResponse).data))
+            assertThat(result.data, IsEqual(Resource.Success(conversionResponse).data))
+        }
+    }
+
+    @Test
+    fun `get converted currency value should return success response`() {
+        runTest {
+            `when`(converterRepository.getCurrencyValue("USD", "KES", 1.0))
+                .thenReturn(Resource.Success(currencyResponse))
+            viewModel.getCurrencyValue("USD", "KES", 1.0)
+            testDispatcher.scheduler.advanceUntilIdle()
+            val result =  viewModel.currencyResponseState.getOrAwaitValue()
+            assertThat(result.data, IsEqual(Resource.Success(currencyResponse).data))
         }
     }
 
