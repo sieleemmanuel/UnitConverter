e4a1925ec39627cb18569e01d5628540ea7d4225
kimsiele <sielekim.emmanuel@gmail.com>
2023-01-10 23:13:03 +0300

    test currency converter response

--- app/src/test/java/com/siele/unitconverter/repository/RepositoryTest.kt
+++ app/src/test/java/com/siele/unitconverter/repository/RepositoryTest.kt
@@ -2,6 +2,7 @@ package com.siele.unitconverter.repository
 
 import com.siele.unitconverter.data.api.ConverterApi
 import com.siele.unitconverter.data.model.ConversionResponse
+import com.siele.unitconverter.data.model.CurrencyResponse
 import com.siele.unitconverter.data.repository.DefaultConverterRepository
 import kotlinx.coroutines.runBlocking
 import org.hamcrest.MatcherAssert.assertThat
@@ -12,6 +13,7 @@ import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 import org.mockito.Mock
 import org.mockito.Mockito
+import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
 import retrofit2.Response
 
@@ -24,12 +26,12 @@ class RepositoryTest {
 
     @Before
     fun setup() {
-        MockitoAnnotations.initMocks(this)
+        MockitoAnnotations.openMocks(this)
         defaultConverterRepository = DefaultConverterRepository(converterApi)
     }
 
     @Test
-    fun `get converted value test`() {
+    fun `get converted unit value test should return succcess response`() {
         runBlocking {
             val conversionResponse =
                 ConversionResponse(
@@ -40,9 +42,27 @@ class RepositoryTest {
                     "KES",
                     true
                 )
-            Mockito.`when`(converterApi.getValue()).thenReturn(Response.success(conversionResponse))
-            val response = defaultConverterRepository.getConvertedValue("", "", "")
+            `when`(converterApi.getValue()).thenReturn(Response.success(conversionResponse))
+            val response = defaultConverterRepository.getConvertedValue("", "", "", true)
             assertThat(conversionResponse, IsEqual(response.data))
+        }
+    }
+
+    @Test
+    fun `get converted currency value should return success response`(){
+        runBlocking {
+            val currencyResponse = CurrencyResponse(
+                "USD",
+                1.0,
+                123.05,
+                "KES"
+            )
+
+            `when`(converterApi.getCurrencyValue("USD", "KES", 1.0))
+                .thenReturn(Response.success(currencyResponse))
+
+            val response  = defaultConverterRepository.getCurrencyValue("USD", "KES", 1.0)
+            assertThat(currencyResponse.new_currency,IsEqual(response.data!!.new_currency))
         }
     }
 }
