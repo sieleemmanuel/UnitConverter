8dc5a1e8bf5993bef67b8159bcee77c9e4aeea17
kimsiele <sielekim.emmanuel@gmail.com>
2023-01-11 08:57:12 +0300

    code refactor and cleaning

--- app/src/main/java/com/siele/unitconverter/data/api/ConverterApi.kt
+++ app/src/main/java/com/siele/unitconverter/data/api/ConverterApi.kt
@@ -8,31 +8,31 @@ import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 import retrofit2.http.Url
-import java.time.temporal.TemporalAmount
 
 interface ConverterApi {
     @GET
     suspend fun getValue(
         @Url url: String = Constants.BASE_URL,
         @Query("user-id")
-        userId:String = BuildConfig.USER_ID,
+        userId: String = BuildConfig.USER_ID,
         @Query("api-key")
-        apiKey:String = BuildConfig.NEUTRINO_API_PROD,
+        apiKey: String = BuildConfig.NEUTRINO_API_PROD,
         @Query("from-value")
-        fromValue:String ="",
+        fromValue: String = "",
         @Query("from-type")
-        fromType:String ="",
+        fromType: String = "",
         @Query("to-type")
-        toType:String ="",
-    ):Response<ConversionResponse>
+        toType: String = "",
+    ): Response<ConversionResponse>
 
     @GET
-    suspend fun getCurrencyValue(@Url url: String = Constants.CURRENCY_BASE_URL,
-                                 @Query ("have")
-        have:String = "",
-                                 @Query ("want")
-        want:String = "",
-                                 @Query ("amount")
+    suspend fun getCurrencyValue(
+        @Url url: String = Constants.CURRENCY_BASE_URL,
+        @Query("have")
+        have: String = "",
+        @Query("want")
+        want: String = "",
+        @Query("amount")
         amount: Double
-    ):Response<CurrencyResponse>
+    ): Response<CurrencyResponse>
 }
--- app/src/test/java/com/siele/unitconverter/repository/FakeConverterRepository.kt
+++ app/src/test/java/com/siele/unitconverter/repository/FakeConverterRepository.kt
@@ -1,6 +1,7 @@
 package com.siele.unitconverter.repository
 
 import com.siele.unitconverter.data.model.ConversionResponse
+import com.siele.unitconverter.data.model.CurrencyResponse
 import com.siele.unitconverter.data.repository.ConverterRepository
 import com.siele.unitconverter.util.Resource
 
@@ -19,12 +20,21 @@ class FakeConverterRepository : ConverterRepository{
     override suspend fun getConvertedValue(
         fromValue: String,
         fromType: String,
-        toType: String
+        toType: String,
+        isConnected: Boolean
     ): Resource<ConversionResponse> {
         return if(isError) {
             Resource.Error("Error", null)
         }else{
             Resource.Success(conversionResponse)
         }
+    }
+
+    override suspend fun getCurrencyValue(
+        have: String,
+        want: String,
+        amount: Double
+    ): Resource<CurrencyResponse> {
+        TODO("Not yet implemented")
     }
 }
